b2c7b34aa5161262a4fcea1b98b4e153c0e5a44d
Sel007 <selina.08225.1@gmail.com>
2020-08-12 18:36:34 -0400

    Added main.js file, Projects page

--- main.js
+++ main.js
@@ -4,5 +4,23 @@ $(document).ready(function () {
     $('.submit').click(function (event) {
         event.preventDefault ()
         console.log('Clicked button')
+
+        var email = $('.email').val()
+        var subject = $('.subject').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if(email.length > 5 && email.includes('@') && email.includes('.'))
+        {
+            console.log('Email is valid')
+        }
+        
+        else
+        {
+            console.log('Email is not valid')
+        }
+        
+        
     })
 })
