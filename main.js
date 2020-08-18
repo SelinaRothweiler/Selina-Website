fa87f8ec55b13288635706bde4ab0d657d2ce042
Sel007 <selina.08225.1@gmail.com>
2020-08-17 20:04:04 -0400

    Updated Projects, Contact html and css by removing icons and adjusting form, moved package.json and server.js files, Modified package-lock.json and main.js

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
-console.log()
+const { default: Axios } = require("axios");
+const { json } = require("body-parser");
 
-$(document).ready(function () {
-    $('.submit').click(function (event) {
-        event.preventDefault ()
-        console.log('Clicked button')
-
-        var email = $('.email').val()
-        var subject = $('.subject').val()
-        var message = $('.message').val()
-        var statusElm = $('.status')
-        statusElm.empty()
-
-        if(email.length > 5 && email.includes('@') && email.includes('.'))
-        {
-            console.log('Email is valid')
+class myForm {
+    const (_props) {
+        this.state = {
+            name:'',
+            number:'',
+            email:'',
+            subject:'',
+            message:'',
+            disabled: false,
+            emailSent: null,
+        };
+        this.setState = undefined;
+    }
+    handleChange = (event) => {
+        console.log(event);
+        const target= event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            disabled:true
+        });
+        Axios.post('/api/email',this.state)
+            .then(_res => {
+                if(_res.data.success){
+                    this.setState({
+                    disabled:false,
+                    emailSent: true
+                });
+                }
+                else{
+                    this.setState({
+                        disabled:false,
+                        emailSent:false                   
+                     });
+                }
+            })
+            .catch(_err=> {
+                this.setState({
+                    disabled: false,
+                    emailSent: false
+                });
+             })
         }
-        
-        else
-        {
-            console.log('Email is not valid')
-        }
-        
-        
-    })
-})
+    }
